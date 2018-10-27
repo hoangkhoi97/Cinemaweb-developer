2d620be83460049634c3ca7fda3c04936276052f
khoi <khoi.dinh@gumiviet.com>
2018-10-27 09:04:44 +0700

    Update Font-end

--- js/slot.js
+++ js/slot.js
@@ -1,4 +1,25 @@
-function buttonBookingSlot(a) {
-    var x = document.getElementsByClassName('turn_on')
+function onSeatClick(btn) {
+    var a = document.getElementsByClassName("on");
+
+    if (document.getElementById(btn.id).classList.contains('on'))
+    {
+        btn.classList.remove("on");
+        btn.classList.add("off");
+        document.getElementById("slot").innerHTML = " ";
+
+    }
+    else {
+
+        if(a.length > 0) {
+            for(let i = 0; i < a.length; ++i){
+                a[i].classList.remove("on");
+            }
+        }
+
+        btn.classList.remove("off");
+        btn.classList.add("on");
+        document.getElementById("slot").innerHTML = btn.id;
+
+    }
 
 }
